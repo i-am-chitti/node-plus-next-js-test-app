68528f239617450b20508f89681980f95d31286a
Deepak Kumar <cu.18bcs2295@gmail.com>
2023-05-29 02:42:38 +0530

    Update endpoint

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ import express from 'express';
 const PORT = 5001;
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
 	res.send('I am backend server');
+});
+
+app.get('/api/auth', (req, res) => {
+	return res.json({
+		status: 'SUCCESS',
+		message: 'Hello'
+	})
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
